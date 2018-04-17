362127e5768c30da0629895c3c5c53089b6a176b
SPIR3 <32450745+SPIR3@users.noreply.github.com>
2018-04-17 15:18:53 +0100

    update

--- DiceRoll/js/input.js
+++ DiceRoll/js/input.js
@@ -1,17 +1,58 @@
 
 $(document).on("pagecreate","#homepage",function(){
+    
+    alert ("Homepage created!");
   
   	$('#dice').on("tap",function(){
-	alert "Go to dice roll!";
+	alert ("Go to dice roll!");
+    window.location.href = "#dicePage";
  	});                       
 
 	$('#coin').on("tap",function(){
-	alert "Go to coin flip!";
+	alert ("Go to coin flip!");
+    window.location.href = "#coinPage";
  	}); 
 
 	$('#eight').on("tap",function(){
-    alert "Go to Magic 8 Ball!";
+    alert ("Go to Magic 8 Ball!");
+    window.location.href = "#magicPage";
   	});   
 
 
+});
+
+//dice roll page
+$(document).on("pagecreate","#dicePage",function(){
+    
+    alert ("dicePage created!");
+    
+  	$('#Backdice').on("tap",function(){
+	alert ("Go to homepage!");
+    window.location.href = "#homepage";
+ 	}); 
+    
+});
+
+//coinflip page
+$(document).on("pagecreate","#coinPage",function(){
+    
+    alert ("coinPage created!");
+    
+  	$('#Backcoin').on("tap",function(){
+	alert ("Go to homepage!");
+    window.location.href = "#homepage";
+ 	}); 
+    
+});
+
+// magic 8 page
+$(document).on("pagecreate","#magicPage",function(){
+    
+    alert ("magicPage created!");
+    
+  	$('#Backmagic').on("tap",function(){
+	alert ("Go to homepage!");
+    window.location.href = "#homepage";
+ 	}); 
+    
 });
